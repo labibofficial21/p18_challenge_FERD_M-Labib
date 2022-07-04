69820ce1da7dfa038a59f82a2f867a509fe80f7f
labibofficial21 <labibofficial21@gmail.com>
2022-07-04 18:40:40 +0700

    P18_Challenge_FERD_M Labib

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Image
+        style={styles.img}
+        source={{
+          uri: 'https://www.shareicon.net/data/256x256/2016/07/08/117367_logo_512x512.png'
+        }} />
+      <View style={styles.container}>
+        <Text>Mari belajar React Native</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
@@ -17,4 +26,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txt: {
+    fontSize: 30,
+  },
+  img: {
+    height: 100,
+    width: 100,
+  }
 });
